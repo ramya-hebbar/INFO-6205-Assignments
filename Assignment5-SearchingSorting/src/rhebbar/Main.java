a7d59718a439a85c3bdefc542dc5dbfdb5ea6a6b
Ramya Hebbar <hebbar.r@husky.neu.edu>
2021-03-04 00:45:49 -0800

    Solutions to Q2, Q3 and Q5

--- Assignment5-SearchingSorting/src/rhebbar/Main.java
+++ Assignment5-SearchingSorting/src/rhebbar/Main.java
@@ -4,7 +4,98 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        
+
+        SearchSort ss = new SearchSort();
+
+        printSeparator();
+
+        ////
+        //Q1: Find K Closest Elements
+        System.out.println("Question 1 - Find K closest elements");
+
+        printSeparator();
+
+        /////
+        //Q2: Find Intersection of 2 sorted arrays
+        System.out.println("Question 2 - Find intersection of 2 sorted arrays");
+
+        System.out.println("Pass 1 :");
+
+        int[] array1 = new int[] {4,9,5};
+        System.out.print("Array 1 - ");
+        ss.print(array1);
+
+        int[] array2 = new int[] {9,4,9,8,4};
+        System.out.print("Array 2 - ");
+        ss.print(array2);
+
+        int[] intersection = ss.getIntersectionOfArrays(array1, array2);
+        System.out.print("Intersection - ");
+        ss.print(intersection);
+
+        System.out.println("Pass 2 : ");
+
+        array1 = new int[] {1, 2, 2, 1};
+        System.out.print("Array 1 - ");
+        ss.print(array1);
+
+        array2 = new int[] {2, 2};
+        System.out.print("Array 2 - ");
+        ss.print(array2);
+
+        intersection = ss.getIntersectionOfArrays(array1, array2);
+        System.out.print("Intersection - ");
+        ss.print(intersection);
+
+        printSeparator();
+
+        /////
+        //Q3: Peak Index in a mountain
+        System.out.println("Question 3 - Peak Index in a mountain");
+
+        System.out.println("Pass 1 :");
+
+        int[] arr = new int[] {0,1,0};
+        System.out.print("Array is - ");
+        ss.print(arr);
+
+        int peakIndex = ss.getPeakIndexInArray(arr);
+        System.out.println("Peak is at index " + peakIndex);
+
+        System.out.println("Pass 2 :");
+
+        arr = new int[] {24,69,100,99,79,78,67,36,26,19};
+        System.out.print("Array is - ");
+        ss.print(arr);
+
+        peakIndex = ss.getPeakIndexInArray(arr);
+        System.out.println("Peak is at index " + peakIndex);
+
+        printSeparator();
+
+        /////
+        //Q4: Reorganize string
+        System.out.println("Question 4 - Reorganize string");
+
+        printSeparator();
+
+        /////
+        //Q5: Count number of 1's in sorted binary array
+        System.out.println("Question 5 - Count number of 1's in sorted binary array");
+
+        System.out.println("Pass 1 :");
+
+        int[] binArr = { 1, 1, 1, 1, 0, 0, 0 };
+        int n = binArr.length;
+        System.out.println(ss.countOnes(binArr, 0, n-1) + " number of 1s found in the array");
+
+        System.out.println("Pass 2 :");
+
+        binArr = new int[] {1, 1, 1, 1, 1, 1, 1};
+        n = binArr.length;
+        System.out.println(ss.countOnes(binArr, 0, n-1) + " number of 1s found in the array");
+
+        printSeparator();
     }
 
     public static void printSeparator() {
--- Assignment5-SearchingSorting/src/rhebbar/SearchSort.java
+++ Assignment5-SearchingSorting/src/rhebbar/SearchSort.java
@@ -1,10 +1,108 @@
 package rhebbar;
 
+import java.util.*;
+
 public class SearchSort {
     //Wrapper functions
 
+    public void print(int[] arr) {
+        printArray(arr);
+    }
+
+    public int[] getIntersectionOfArrays(int[] arr1, int[] arr2) {
+        return intersection(arr1, arr2);
+    }
+
+    public int getPeakIndexInArray(int[] array) {
+        return peakIndexInMountainArray(array);
+    }
+
+    public int countOnes(int[] arr, int low, int high) {
+        return countNumberOfOnes(arr, low, high);
+    }
+
     //Solution functions
 
-    //Helper functions
+    //Q2
+    private int[] intersection(int[] arr1, int[] arr2) {
+        HashSet<Integer> setOfArr1 = new HashSet<Integer>();
 
+        for(int i=0; i<arr1.length; i++) {
+            setOfArr1.add(arr1[i]);
+        }
+
+        HashSet<Integer> setOfArr2 = new HashSet<Integer>();
+
+        for(int i=0; i<arr2.length; i++) {
+            setOfArr2.add(arr2[i]);
+        }
+
+        setOfArr1.retainAll(setOfArr2);
+
+        int[] intersectionSet = new int[setOfArr1.size()];
+        int j = 0;
+
+        for(Integer i : setOfArr1) {
+            intersectionSet[j++] = i;
+        }
+
+        return intersectionSet;
+    }
+
+    //Q3
+    private int peakIndexInMountainArray(int[] arr) {
+        if(arr.length < 3)
+            return -1;
+
+        int index = -1;
+
+        for(int i=1; i<arr.length-1; i++) {
+            if((arr[i-1] < arr[i]) && (arr[i] > arr[i+1]))
+            {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    //Q4
+
+    //Q5
+    private int countNumberOfOnes(int[] arr, int low, int high) {
+        while(low <= high) {
+            int mid = (low+high)/2;
+
+            //check if the element at the middle index is the last 1
+            if ((mid == high || arr[mid + 1] == 0) && (arr[mid] == 1))
+                return mid + 1;
+
+            // If element is not last 1, check on the right side
+            if (arr[mid] == 1)
+                return countNumberOfOnes(arr, (mid + 1), high);
+
+            // else check on the left side
+            return countNumberOfOnes(arr, low, (mid - 1));
+        }
+
+        return 0;
+    }
+
+    //Helper functions
+    private void printArray(int[] array) {
+        System.out.print("[");
+
+        for (int i = 0; i < array.length; i++) {
+            if(i == array.length - 1) {
+                System.out.print(array[i]);
+            }
+            else {
+                System.out.print(array[i] + ", ");
+            }
+        }
+
+        System.out.print("]");
+        System.out.println();
+    }
 }
